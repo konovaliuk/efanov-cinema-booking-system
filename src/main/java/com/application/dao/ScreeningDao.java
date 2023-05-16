340bc8dc578f620a1a52a0dad61b67d7e86f2e48
Iliya Efanov <84502015+JeskoThe1@users.noreply.github.com>
2023-05-16 16:36:59 +0300

    Delete src/main directory

--- src/main/java/com/application/dao/ScreeningDao.java
+++
@@ -1,8 +0,0 @@
-package com.application.dao;
-
-import com.application.model.Movie;
-import com.application.model.Screening;
-
-public interface ScreeningDao extends Dao<Screening>{
-    Movie getFilmScreening(long filmId);
-}
--- src/main/java/com/application/dao/SeatDao.java
+++
@@ -1,6 +0,0 @@
-package com.application.dao;
-
-import com.application.model.Seat;
-
-public interface SeatDao extends Dao<Seat>{
-}
